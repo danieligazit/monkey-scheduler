af40c2352957d818540a028758876c51753ecb92
Daniel Gazit <danieligazit@gmail.com>
2021-12-12 02:12:35 +0200

    No real progress

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
+from enum import Enum
 from typing import List
 
-from google.protobuf.type_pb2 import Enum
 from ortools.sat.python import cp_model
 
 THURSDAY = 3
@@ -16,7 +16,7 @@ def date_range(start_date: datetime, end_date: datetime):
         yield start_date + timedelta(n)
 
 
-class ShiftKind:
+class ShiftKind(Enum):
     WEEKDAY = 'Weekday'
     WEEKEND = 'Weekend'
     HOLIDAY = 'Holiday'
@@ -26,7 +26,7 @@ class ShiftKind:
 class Shift:
     start_date: datetime
     end: datetime
-    kind: str
+    kind: ShiftKind
 
 
 def create_shifts(start_date: datetime, end_date: datetime) -> List[Shift]:
@@ -51,6 +51,81 @@ def create_shifts(start_date: datetime, end_date: datetime) -> List[Shift]:
         ))
 
     return shifts
+
+
+def negated_bounded_span(works, start, length):
+    """Filters an isolated sub-sequence of variables assined to True.
+  Extract the span of Boolean variables [start, start + length), negate them,
+  and if there is variables to the left/right of this span, surround the span by
+  them in non negated form.
+  Args:
+    works: a list of variables to extract the span from.
+    start: the start to the span.
+    length: the length of the span.
+  Returns:
+    a list of variables which conjunction will be false if the sub-list is
+    assigned to True, and correctly bounded by variables assigned to False,
+    or by the start or end of works.
+  """
+    sequence = []
+    # Left border (start of works, or works[start - 1])
+    if start > 0:
+        sequence.append(works[start - 1])
+    for i in range(length):
+        sequence.append(works[start + i].Not())
+    # Right border (end of works or works[start + length])
+    if start + length < len(works):
+        sequence.append(works[start + length])
+    return sequence
+
+
+def add_soft_sequence_constraint(model: cp_model.CpModel, works, hard_min, soft_min, min_cost,
+                                 soft_max, hard_max, max_cost, prefix):
+    """Sequence constraint on true variables with soft and hard bounds.
+    This constraint look at every maximal contiguous sequence of variables
+    assigned to true. If forbids sequence of length < hard_min or > hard_max.
+    Then it creates penalty terms if the length is < soft_min or > soft_max.
+    Args:
+    model: the sequence constraint is built on this model.
+    works: a list of Boolean variables.
+    hard_min: any sequence of true variables must have a length of at least
+      hard_min.
+    soft_min: any sequence should have a length of at least soft_min, or a
+      linear penalty on the delta will be added to the objective.
+    min_cost: the coefficient of the linear penalty if the length is less than
+      soft_min.
+    soft_max: any sequence should have a length of at most soft_max, or a linear
+      penalty on the delta will be added to the objective.
+    hard_max: any sequence of true variables must have a length of at most
+      hard_max.
+    max_cost: the coefficient of the linear penalty if the length is more than
+      soft_max.
+    prefix: a base name for penalty literals.
+    Returns:
+    a tuple (variables_list, coefficient_list) containing the different
+    penalties created by the sequence constraint.
+    """
+    cost_literals = []
+    cost_coefficients = []
+
+    spans = []
+    for length in range(len(works))[4:5]:
+        for start in range(len(works) - length + 1):
+            span = negated_bounded_span(works, start, length)
+            name = f': over_span(start={start}, length={length})'
+            span = model.New(span)
+
+            spans.append(span)
+    print(spans)
+    model.Maximize(sum(spans))
+
+
+def add_sequence_constraint(model, works, hard_min):
+    # Forbid sequences that are too short.
+    for length in range(1, hard_min):
+        for start in range(len(works) - length - 1):
+            print(span)
+            model.AddBoolOr(negated_bounded_span(works, start, length))
 
 
 def main():
@@ -88,23 +163,31 @@ def main():
 
     for monkey in all_monkeys:
         num_shifts_worked = 0
-        for shift in all_shifts:
+        for index, shift in enumerate(all_shifts):
             num_shifts_worked += shifts[(monkey, shift)]
 
         model.Add(min_shifts_per_monkey <= num_shifts_worked)
         model.Add(num_shifts_worked <= max_shifts_per_monkey)
 
-    # for monkey in all_monkeys:
-    #     days_since_last_shift = []
-    #
-    #     for i, shift in enumerate(all_shifts):
-    #         for previous_shift in all_shifts[:i]:
-    #
-    #             days_since_last_shift = model.NewIntVar(1, len(num_shifts))
-    #             shifts[(monkey, shift)]
-    #
-    #             model.Maximize()
-    #             days_since_last_shift += shifts[(monkey, shift)]
+    for monkey in all_monkeys:
+        works = [shifts[monkey, shift] for shift in all_shifts]
+
+        add_sequence_constraint(model, works, 1)
+        hard_min, soft_min, min_cost, soft_max, hard_max, max_cost = (1, 2, 20, 3, 4, 5)
+
+        add_soft_sequence_constraint(
+            model, works, hard_min, soft_min, min_cost, soft_max, hard_max,
+            max_cost, f'shift_constraint(employee {monkey}, shift {shift})')
+
+        # days_since_last_shift = []
+        #
+        # for i, shift in enumerate(all_shifts):
+        #     for previous_shift in all_shifts[:i]:
+        #         days_since_last_shift = model.NewIntVar(1, len(num_shifts))
+        #         shifts[(monkey, shift)]
+        #
+        #         model.Maximize()
+        #         days_since_last_shift += shifts[(monkey, shift)]
 
     # model.Maximize(
     #     sum(shift_requests[n][d][s] * shifts[(n, d, s)] for n in all_nurses
